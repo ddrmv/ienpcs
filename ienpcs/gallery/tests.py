ef9a6c32d8e6f9f3668bf8ef013463acd5065feb
ddrmv <ddramchev@gmail.com>
2024-01-22 01:25:55 +0000

    Add test for Game model

--- ienpcs/gallery/tests.py
+++ ienpcs/gallery/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Game
+
+class GalleryModelTests(TestCase):
+    def test_game_gets_created(self):
+        name = "Baldur's Gate"
+        codename = "bg1"
+        game = Game(name=name, codename=codename)
+        self.assertEqual(game.codename, codename)
