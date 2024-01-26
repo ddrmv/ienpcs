c2bc11edf4959f918a618a8b5199e08271bd53f2
ddrmv <ddramchev@gmail.com>
2024-01-26 10:47:48 +0000

    Implement navbar links, active, add placeholder templates for pages

--- ienpcs/gallery/urls.py
+++ ienpcs/gallery/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", views.GameListView.as_view(), name="game_list"),
+    path("games/", views.GameListView.as_view(), name="game_list"),
+    path("links/", views.link_list, name="link_list"),
+    path("about/", views.about, name="about"),
+    path("characters/", views.CharacterListView.as_view(), name="character_list"),
     path("game/<slug:slug>", views.game_detail, name="game-detail"),
     path("toggle_theme", views.toggle_theme, name="toggle_theme"),
 ]
--- ienpcs/gallery/views.py
+++ ienpcs/gallery/views.py
@@ -5,8 +5,8 @@ from django.views import generic
 from .models import Game, NpcInGame
 
 
-class IndexView(generic.ListView):
-    template_name = "gallery/index.html"
+class GameListView(generic.ListView):
+    template_name = "gallery/game_list.html"
     context_object_name = "game_list"
     queryset = Game.objects.all()
 
@@ -25,6 +25,22 @@ def game_detail(request, slug):
     return render(
         request, "gallery/game_detail.html", {"game": game, "origin_dict": origin_dict}
     )
+
+
+class CharacterListView(generic.ListView):
+    template_name = "gallery/character_list.html"
+    context_object_name = "character_list"
+    queryset = Game.objects.all()
+
+
+def link_list(request):
+    link_list_dict = {"link1": "placeholder1", "link2": "placeholder2"}
+    context = {"link_list_dict": link_list_dict}
+    return render(request, "gallery/link_list.html", context)
+
+
+def about(request):
+    return render(request, "gallery/about.html", {})
 
 
 def toggle_theme(request):
