844b24c013783af55bd6a10d3fa7f0ab5d1cc9b2
ddrmv <ddramchev@gmail.com>
2024-01-28 10:47:13 +0000

    Add admin button to navbar for internal IP address

--- ienpcs/ienpcs/settings.py
+++ ienpcs/ienpcs/settings.py
@@ -125,5 +125,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
+INTERNAL_IPS = ("127.0.0.1",)
